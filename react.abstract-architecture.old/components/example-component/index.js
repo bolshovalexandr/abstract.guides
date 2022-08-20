36bbc72fc4744015176421a0411d5f2bc44c309f
Litest <bolshov@inbox.ru>
2022-08-20 14:42:35 +0300

    ADD old react guids (2017)

---
+++ react.abstract-architecture.old/components/example-component/index.js
@@ -0,0 +1,6 @@
+export { default } from './example-component';
+
+require('./__image/example-component_image.scss');
+require('./__title/example-component__title.scss');
+
+require('./example-component.scss');
